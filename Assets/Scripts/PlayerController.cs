2b2c749e7959bee272e9e4aa0bd812356d821131
AvilCavill <alexavilahenarejos57@gmail.com>
2024-11-06 13:25:43 +0100

    Fix del PLayerControl de la camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
+    private bool playerIsWalking = false;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 10.0f;
+    public float playerMoveSpeed;
+    private float playerWalkSpeed = 15.0f;
+    private float playerSprintSpeed = 25.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+    
+    public StaminaController _staminaController;
+
+    public Transform cameraTransform;
+
     // Start is called before the first frame update
     void Start()
     {
+        _staminaController = gameObject.GetComponent<StaminaController>();
         controller = gameObject.AddComponent<CharacterController>();
+
+        if (Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+    }
+
+    public void SetRunSpeed(float speed)
+    {
+        playerSprintSpeed = speed;
     }
 
     // Update is called once per frame
@@ -25,21 +44,48 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            gameObject.transform.forward = move;
+            playerMoveSpeed = playerSprintSpeed;
+        }
+        else 
+        {
+            playerMoveSpeed = playerWalkSpeed;
+        }
+        // Obtener axis
+        Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (playerIsWalking)
+        {
+            _staminaController.playerIsSprinting = false;
         }
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        // if (!playerVelocity &&)
+        // {
+        //     if(playerMoveSpeed.)
+        // }
+        // Ejecutar solo si hay input del jugador
+        if (inputDirection.magnitude > 0.1f)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
+            // Convertir la dirección de entrada en relación a la dirección de la cámara
+            Vector3 moveDirection =
+                cameraTransform.right * inputDirection.x + cameraTransform.forward * inputDirection.z;
+            moveDirection.y = 0f; // Asegurarse de que no se mueva en el eje Y
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime); 
+            // Mover el jugador
+            controller.Move(moveDirection * Time.deltaTime * playerMoveSpeed);
+
+            // Rotar el jugador hacia la dirección de movimiento
+            transform.forward = moveDirection;
+
+            // Changes the height position of the player..
+            if (Input.GetButtonDown("Jump") && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+        }
     }
 }
